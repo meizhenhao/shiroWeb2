f816ef3c88cc426adbb219f1d30b3a1864d63b05
Mei Zhenhao <2323794414@qq.com>
2019-01-03 13:28:25 +0800

    add all files

---
+++ src/main/java/com/java1234/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.java1234.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.java1234.entity.User;
+
+
+
+/**
+ * 用户Controller层
+ * @author Administrator
+ *
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	
+	/**
+	 * 用户登录
+	 * @param user
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("/login")
+	public String login(User user,HttpServletRequest request){
+		Subject subject=SecurityUtils.getSubject();
+		UsernamePasswordToken token=new UsernamePasswordToken(user.getUserName(), user.getPassword());
+		try{
+			subject.login(token);
+			Session session=subject.getSession();
+			System.out.println("sessionId:"+session.getId());
+			System.out.println("sessionHost:"+session.getHost());
+			System.out.println("sessionTimeout:"+session.getTimeout());
+			session.setAttribute("info", "session的数据");
+			return "redirect:/success.jsp";
+		}catch(Exception e){
+			e.printStackTrace();
+			request.setAttribute("user", user);
+			request.setAttribute("errorMsg", "用户名或密码错误！");
+			return "index";
+		}
+	}
+	
+
+}
---
+++ src/main/java/com/java1234/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.java1234.service.impl;
+
+import java.util.Set;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.java1234.dao.UserDao;
+import com.java1234.entity.User;
+import com.java1234.service.UserService;
+
+@Service("userService")
+public class UserServiceImpl implements UserService{
+
+	@Resource
+	private UserDao userDao;
+	
+	public User getByUserName(String userName) {
+		return userDao.getByUserName(userName);
+	}
+
+	public Set<String> getRoles(String userName) {
+		return userDao.getRoles(userName);
+	}
+
+	public Set<String> getPermissions(String userName) {
+		return userDao.getPermissions(userName);
+	}
+
+}
